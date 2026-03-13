cb4870245c4737e272e60fc486980064bde12653
ZidYu <zecyu@mymail.mapua.edu.ph>
2026-03-13 22:46:14 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
-    return redirect()->route('login');
+    return redirect()->route('login.blade.php');
 });
 
 Route::get('/login', function () {
